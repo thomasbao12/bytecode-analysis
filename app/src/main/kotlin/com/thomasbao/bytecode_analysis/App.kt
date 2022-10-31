a172ea2180ba6a81ae8d7c314364a077eeced88c
Thomas Bao <thomas.bao@airbnb.com>
2022-10-31 15:07:58 -0700

    floyd warshall

--- app/src/main/kotlin/com/thomasbao/bytecode_analysis/App.kt
+++ app/src/main/kotlin/com/thomasbao/bytecode_analysis/App.kt
@@ -3,42 +3,65 @@
  */
 package com.thomasbao.bytecode_analysis
 
+import org.apache.bcel.classfile.ClassParser
+import org.apache.bcel.classfile.ConstantClass
+import org.jgrapht.graph.DefaultDirectedGraph
+import org.jgrapht.graph.DefaultEdge
+import org.jgrapht.Graph
+import org.jgrapht.alg.shortestpath.FloydWarshallShortestPaths
 import java.nio.file.Files
 import java.nio.file.Paths
-import org.apache.bcel.classfile.ConstantClass
-import org.apache.bcel.classfile.ConstantUtf8
-import org.apache.bcel.classfile.ClassParser
-import kotlin.io.path.pathString
-import kotlin.io.path.writeLines
+import kotlin.io.path.absolutePathString
+import kotlin.io.path.relativeTo
 
-class App {
-    val greeting: String
-        get() {
-            return "Hello World!"
-        }
-}
 
-fun getConstantPoolClassRefs(filepath: String):List<ConstantUtf8> {
+val AIRBNB_PACKAGE = "com/airbnb"
+
+fun getConstantPoolClassRefs(filepath: String): Set<String> {
   val classParser = ClassParser(filepath)
   val javaClass = classParser.parse()
   val constantPool = javaClass.constantPool
   return constantPool.map { it as? ConstantClass }.filterNotNull().map {
-    constantPool.getConstantUtf8(it.nameIndex)
-  }
+    constantPool.getConstantUtf8(it.nameIndex).bytes
+  }.toSet()
+}
+
+class Klass(val classPath: String, val classRefs: Set<String>)
+
+fun getKlassFromFile(classPath: String, classRefs: Set<String>): Klass {
+    return Klass(
+      classPath,
+      classRefs.filter { it.startsWith(AIRBNB_PACKAGE) && it != classPath }.toSet()
+    )
 }
 
 fun main(args: Array<String>) {
     val inputDirPath = args[0]
-    val outputDirPath = args[1]
+
+    //val outputDirPath = args[1]
+
+    val g: Graph<String, DefaultEdge> = DefaultDirectedGraph<String, DefaultEdge>(DefaultEdge::class.java)
     Files.walk(
-        Paths.get(inputDirPath)
+        Paths.get(inputDirPath + "/" + AIRBNB_PACKAGE)
     ).forEach {
       if (it.fileName.toString().endsWith(".class")) {
-        println("parsing ${it.fileName}")
-        val classRefs = getConstantPoolClassRefs(it.pathString)
-        val out = Files.createFile(Paths.get(outputDirPath, it.fileName.toString() + ".refs"))
-        out.writeLines(classRefs.map { it.toString() } )
+        val classPath = it.toAbsolutePath().relativeTo(Paths.get(inputDirPath)).toString().removeSuffix(".class")
+        println("parsing ${classPath}")
+
+        val classRefs = getConstantPoolClassRefs(it.absolutePathString())
+
+
+
+        g.addVertex(classPath)
+        classRefs.filter { it.startsWith(AIRBNB_PACKAGE) && it != classPath }.forEach {
+          g.addVertex(it)
+          g.addEdge(classPath, it)
+        }
       }
     }
+
+  val floydWarshall = FloydWarshallShortestPaths(g)
+  println("Graph has ${g.vertexSet().size} nodes and ${g.edgeSet().size} edges")
+  println("ShortestsPaths count: ${floydWarshall.shortestPathsCount})")
 }
 
